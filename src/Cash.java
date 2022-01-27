6ec907894bbf7f34edee7739610cba0e9348362f
Ali Ahmed <alimohdapg@gmail.com>
2022-01-27 14:56:06 +0000

    Added test directory, implemented Cash and CashTest classes
    Notes: tests use JUnit 5, cash variable of type int in Cash class was replaced with the amount variable of type int to avoid confusion between class and variable.

--- src/Cash.java
+++ src/Cash.java
@@ -1,4 +1,24 @@
-public class Cash
-{
+public class Cash {
 
+    private int amount;
+
+    public Cash(int amount){
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void addAmount(int amount) {
+        this.amount += amount;
+    }
+
+    public void subtractAmount(int amount) {
+        this.amount -= amount;
+    }
 }
---
+++ test/CashTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CashTest {
+
+    private Cash cash;
+
+    @BeforeEach
+    void setUp(){
+        cash = new Cash(1000);
+    }
+
+    @Test
+    void testGetAmount() {
+        assertEquals(1000, cash.getAmount());
+    }
+
+    @Test
+    void testSetAmount() {
+        cash.setAmount(987654321);
+        assertEquals(987654321, cash.getAmount());
+    }
+
+    @Test
+    void testAddAmount() {
+        cash.addAmount(500);
+        assertEquals(1500, cash.getAmount());
+    }
+
+    @Test
+    void testSubtractAmount() {
+        cash.subtractAmount(500);
+        assertEquals(500, cash.getAmount());
+    }
+}
