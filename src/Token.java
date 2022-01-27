d60c32f281a6512fc6f7cb5a3b0541c015da60d0
hanzhengong <brandongonghanzhen@gmail.com>
2022-01-27 18:24:47 +0000

    Token Class and Token Tests

--- src/Token.java
+++ src/Token.java
@@ -1,4 +1,29 @@
+/**
+ * An enum class represents the game tokens;
+ * each player is assigned a game token to move around the game board.
+ *
+ * @author Hanzhen Gong
+ */
 public enum Token
 {
+    BOOT("boot"),
+    SMARTPHONE("smartphone"),
+    SHIP("ship"),
+    HATSTAND("hatstand"),
+    CAT("cat"),
+    IRON("iron");
 
+    private final String representation;
+
+    Token(String representation)
+    {
+        this.representation = representation;
+    }
+
+    @Override
+    public String toString()
+    {
+        return representation;
+    }
 }
+
