7256e739a7bff954ffa3086faad2ddd1efdddb49
leon785 <72059671+leon785@users.noreply.github.com>
2022-01-30 22:30:27 +0000

    with javadoc

--- src/Dice.java
+++ src/Dice.java
@@ -1,15 +1,34 @@
-public class Dice{
+/**
+ *
+ * @author Leon
+ */
+public class Dice {
     
     private int dice;
 
+    /**
+     * Constructor
+     *
+     * @param dice the value of dice
+     */
     public Dice(int dice) {
         this.dice = dice;
     }
 
+    /**
+     * Get the value of dice
+     *
+     * @return the value of dice
+     */
     public int getDice() {
         return dice;
     }
 
+    /**
+     * Set the value of dice
+     *
+     * @param dice new value of dice
+     */
     public void setDice(int dice) {
         this.dice = dice;
     }
