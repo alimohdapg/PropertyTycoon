3f6eebf4249bb7bf56975821c610de851cc683eb
leon785 <72059671+leon785@users.noreply.github.com>
2022-01-30 23:15:09 +0000

    rolldice method involved,  javadoc added

--- src/Dice.java
+++ src/Dice.java
@@ -32,4 +32,25 @@ public class Dice {
     public void setDice(int dice) {
         this.dice = dice;
     }
+    
+     /**
+     * Get a random number from an interval
+     * 
+     * @param min the minimum value of the interval
+     * @param max the maximum value of the interval
+     * @return a random number
+     */
+    public static int randomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
+    }
+
+    /**
+     * Roll dice and set the value of the object dice
+     * 
+     * @param d a dice object
+     */
+    public static void rollDice(Dice d) {
+        //generates random integers 1 to 6
+        d.setDice(randomNumber(dice_min, dice_max));
+    }
 }
