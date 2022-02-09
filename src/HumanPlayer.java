e031d9b909f2205d649d264515890b6fe313a20a
leon785 <72059671+leon785@users.noreply.github.com>
2022-02-09 14:31:33 +0000

    Update HumanPlayer.java

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,10 +1,40 @@
 import java.util.ArrayList;
 
-public class HumanPlayer extends Player
-{
+public class HumanPlayer extends Player {
 
-    public HumanPlayer(Token token, int money, ArrayList<Property> properties)
-    {
-        super(token, money, properties);
+    private int location;
+    private boolean inJail;
+    private boolean passedGo;
+
+    public HumanPlayer(Token t, int money, ArrayList<Property> properties){
+        super(t, 1500, properties);
+        location =  0;
+        inJail = false;
+        passedGo = false;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public boolean isInJail() {
+        return inJail;
+    }
+
+    public void setInJail(boolean inJail) {
+        this.inJail = inJail;
+    }
+
+    public boolean isPassedGo() {
+        return passedGo;
+    }
+
+    public void setPassedGo(boolean passedGo) {
+        this.passedGo = passedGo;
     }
 }
+
