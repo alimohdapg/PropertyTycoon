975eba7f5729ddc415cc1f9ed8864f4af35ca55d
hanzhengong <brandongonghanzhen@gmail.com>
2022-03-16 12:46:24 +0000

    (int money) is changed to (Cash money), and the buyProperty method and the sellProperty method have added in the Player abstract class.

--- src/Bank.java
+++ src/Bank.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Bank extends Player {
 
     public Bank(Token token, ArrayList<Property> properties) {
-        super(token, 50000, properties);
+        super(token, new Cash(50000), properties);
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -3,29 +3,46 @@ import java.util.ArrayList;
 public abstract class Player {
 
     private Token token;
-    private int money;
+    private Cash money;
     private ArrayList<Property> properties;
 
-    public Player(Token token, int money, ArrayList<Property> properties) {
+    public Player(Token token, Cash money, ArrayList<Property> properties) {
         this.token = token;
         this.money = money;
         this.properties = properties;
     }
 
-    public int getMoney() {
+    public Cash getMoney() {
         return money;
     }
 
-    public void setMoney(int money) {
+    public void setMoney(Cash money) {
         this.money = money;
     }
 
+    /**
+     * A player buys a property if one has enough cash.
+     *
+     * @param property the property that a player wants to buy
+     */
     public void buyProperty(Property property) {
-
+        if (money.getAmount() >= property.getHouseCost())
+        {
+            money.subtractAmount(property.getHouseCost());
+            properties.add(property);
+        }
     }
 
-    public void sellProperty() {
-
+    /**
+     * A player can sell a property, and the original price will be added back.
+     *
+     * @param property the property that a player wants to sell
+     */
+    public void sellProperty(Property property) {
+        if (properties.contains(property))
+        {
+            money.addAmount(property.getHouseCost());
+        }
     }
 
     public Token getToken() {
