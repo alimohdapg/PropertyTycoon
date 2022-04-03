c65617ff0aacc52244a29c5b8a2af30ad931d314
krdyoung <krd.young@gmail.com>
2022-04-03 13:58:06 +0100

    updating

--- src/GameController.java
+++ src/GameController.java
@@ -1,11 +1,12 @@
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * The controller class for the main game scene
@@ -15,16 +16,8 @@ import java.util.Collections;
 
 public class GameController {
 
-    private final int WIDTH = 1280;
-    private final int HEIGHT = 720;
-
-    private Stage stage;
-
     @FXML
     private Circle playerOneToken;
-
-    @FXML
-    private AnchorPane menuPane;
 
     @FXML
     private Circle p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22,
@@ -35,14 +28,29 @@ public class GameController {
 
     private int current_pos;
 
+    private GameBoard gameBoard;
+    private HumanPlayer player1;
+    private Player[] players;
+
+    private int player1pos;
+
     /**
      * Default function, runs on launch. Initialises the array of positional elements
      */
     public void initialize() {
+
         pos_array = new ArrayList<>();
         Collections.addAll(pos_array, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22,
                 p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39);
         current_pos = 0;
+
+        player1 = new HumanPlayer(Token.CAT, 100, new ArrayList<Property>());
+        players = new Player[1];
+        players[0] = player1;
+
+        player1pos = 0;
+
+        gameBoard = new GameBoard(players);
     }
 
     /**
@@ -51,17 +59,40 @@ public class GameController {
      * @throws IOException
      */
     public void testButtonClicked() throws IOException {
-        System.out.println("Current pos 1: " + current_pos);
         if(current_pos == 39) {
             current_pos = 0;
         } else {
             current_pos++;
-
         }
         playerOneToken.setLayoutX(pos_array.get(current_pos).getLayoutX());
         playerOneToken.setLayoutY(pos_array.get(current_pos).getLayoutY());
-        System.out.println("Current pos 2: " + current_pos);
-
     }
 
+    /**
+     * Simulates taking a turn
+     *
+     * @throws IOException
+     */
+    public void takeTurn() throws InterruptedException {
+        gameBoard.update();
+        System.out.println("The position after rolling is: " + gameBoard.getCurrentPlayerPosition());
+        player1pos = gameBoard.getCurrentPlayerPosition();
+
+        while(current_pos != gameBoard.getCurrentPlayerPosition()) {
+            updatePosition(playerOneToken);
+        }
+    }
+
+    public void updatePosition(Circle token) {
+        System.out.println("Moving...");
+        if(current_pos == 39) {
+            current_pos = 0;
+        } else {
+            current_pos++;
+        }
+        token.setLayoutX(pos_array.get(current_pos).getLayoutX());
+        token.setLayoutY(pos_array.get(current_pos).getLayoutY());
+    }
 }
+
+
