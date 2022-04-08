51575da12a6d27e0318c814a41991dee02582efe
krdyoung <krd.young@gmail.com>
2022-04-08 12:55:29 +0100

    Updated property info on click (click to hide after)

--- src/GameController.java
+++ src/GameController.java
@@ -53,7 +53,7 @@ public class GameController {
 
     @FXML
     private Text property_info_name1, property_info_rent1, property_info_rent_set1, property_info_rent_1house1,
-            property_info_rent_2house1, property_info_rent_3house1, property_info_rent_4house1, property_info_rent_hotel1;
+            property_info_rent_2house1, property_info_rent_3house1, property_info_rent_4house1, property_info_rent_hotel1, property_info_cost1;
 
     @FXML
     private Text notEnoughMoney;
@@ -361,23 +361,18 @@ public class GameController {
         property_info_buy.setVisible(true);
         ArrayList<BoardSpace> board_spaces = gameBoard.getBoardSpaces();
         Property current_property = (Property) board_spaces.get(i);
-        current_property.sellHotel();
-        current_property.setHouseCount(0);
-        property_info_name1.setText(current_property.getName());
+        property_info_name1.setText(fileIO.BoardData.get(i).get(1));
         Color c = Color.web(getHex(i));
         property_info_color1.setFill(c);
+        property_info_cost1.setText("£" + fileIO.BoardData.get(i).get(7));
         property_info_rent1.setText("£" + current_property.getRent());
-        property_info_rent_set1.setText("£" + current_property.getRent() * 2);
-        current_property.setHouseCount(1);
-        property_info_rent_1house1.setText("£" + current_property.getRent());
-        current_property.setHouseCount(2);
-        property_info_rent_2house1.setText("£" + current_property.getRent());
-        current_property.setHouseCount(3);
-        property_info_rent_3house1.setText("£" + current_property.getRent());
-        current_property.setHouseCount(4);
-        property_info_rent_4house1.setText("£" + current_property.getRent());
-        current_property.buyHotel();
-        property_info_rent_hotel1.setText("£" + current_property.getRent());
+        property_info_rent_set1.setText("£" + Integer.parseInt(fileIO.BoardData.get(i).get(8)) * 2);
+
+        property_info_rent_1house1.setText("£" + fileIO.BoardData.get(i).get(10));
+        property_info_rent_2house1.setText("£" + fileIO.BoardData.get(i).get(11));
+        property_info_rent_3house1.setText("£" + fileIO.BoardData.get(i).get(12));
+        property_info_rent_4house1.setText("£" + fileIO.BoardData.get(i).get(13));
+        property_info_rent_hotel1.setText("£" + fileIO.BoardData.get(i).get(14));
     }
 
     /**
