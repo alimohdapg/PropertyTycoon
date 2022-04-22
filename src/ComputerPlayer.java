43c6fb62ca3d67c29bf0036733c55359640f115f
alimohdapg <alimohdapg@gmail.com>
2022-04-22 03:48:00 +0300

    Added rent paying and getting paid functionality.

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 public class ComputerPlayer extends Player {
 
-    public ComputerPlayer(String name, Token token, Cash money, ArrayList<Property> properties, ArrayList<StationAndUtility> stationAndUtilities, Circle playerToken, Text playerName, Text playerMoney) {
-        super(name, token, money, properties, stationAndUtilities, playerToken, playerName, playerMoney);
+    public ComputerPlayer(String name, Token token, ArrayList<Property> properties, ArrayList<StationAndUtility> stationAndUtilities, Circle playerToken, Text playerName, Text playerMoney) {
+        super(name, token, new Cash(1500), properties, stationAndUtilities, playerToken, playerName, playerMoney);
 
     }
 }
